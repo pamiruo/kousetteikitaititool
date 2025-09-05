17336d85fcad2ddb567aa2e602992b806dd0319e
pamiruo <kenkichi0329@gmail.com>
2025-09-06 06:00:18 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from logic.tokyoguru import tokyoguru_handler
 from logic.magireko import magireko_handler
 from logic.monkeyturn5 import monkeyturn5_handler
+from logic.karakuri import karakuri_handler
 
 app = Flask(__name__)
 
@@ -24,6 +25,11 @@ def magireko():
 @app.route("/monkeyturn5", methods=["GET", "POST"])
 def monkeyturn5():
     return monkeyturn5_handler()
+
+# からくりルート
+@app.route("/karakuri", methods=["GET", "POST"])
+def karakuri():
+    return karakuri_handler()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=True)
